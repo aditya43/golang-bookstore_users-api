beefaedb4b65a87452cb16eaef4005704d3d36d3
Aditya Hajare <aditya43@gmail.com>
2021-05-01 13:29:01 +0530

    Create user controller

--- controllers/users/users.go
+++ controllers/users/users.go
@@ -1,9 +1,13 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/aditya43/golang-bookstore_users-api/domain/users"
+	"github.com/aditya43/golang-bookstore_users-api/services"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +17,29 @@ func Get(c *gin.Context) {
 
 func Create(c *gin.Context) {
 	var user users.User
-	c.String(http.StatusNotImplemented, "POST /users")
+
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		// TODO: Handle error
+		fmt.Println(err.Error())
+		return
+	}
+
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		// TODO: Handle json error
+		fmt.Println(err.Error())
+		return
+	}
+	res, err := services.CreateUser(user)
+
+	if err != nil {
+		// TODO: Handle user creation error
+		fmt.Println(err.Error())
+		return
+	}
+
+	c.JSON(http.StatusCreated, res)
 }
 
 func Search(c *gin.Context) {
