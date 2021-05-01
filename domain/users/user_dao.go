19853def987b630de8c5538bc700897351289ffc
Aditya Hajare <aditya43@gmail.com>
2021-05-01 19:11:41 +0530

    Get User Save User

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,1 +1,36 @@
 package users
+
+import (
+	"fmt"
+
+	"github.com/aditya43/golang-bookstore_users-api/utils/errors"
+)
+
+var (
+	usersDB = make(map[int64]*User)
+)
+
+func (user *User) Get() *errors.RESTErr {
+	res := usersDB[user.Id]
+
+	if res == nil {
+		return errors.NotFoundErr(fmt.Sprintf("User Id %d not found", user.Id))
+	}
+
+	return nil
+}
+
+func (user *User) Save() *errors.RESTErr {
+	inDBUser := usersDB[user.Id]
+
+	if inDBUser != nil {
+		if inDBUser.Email == user.Email {
+			return errors.BadRequestErr(fmt.Sprintf("User Email '%s' already exists", user.Email))
+		}
+
+		return errors.BadRequestErr(fmt.Sprintf("User with id %d already exists", user.Id))
+	}
+
+	usersDB[user.Id] = user
+	return nil
+}
