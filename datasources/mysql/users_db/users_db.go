cb8b2ae1989f3f83ee249275fecba9b26576fac2
Aditya Hajare <aditya43@gmail.com>
2021-05-02 14:41:53 +0530

    Get DB credentials from .env

--- datasources/mysql/users_db/users_db.go
+++ datasources/mysql/users_db/users_db.go
@@ -5,17 +5,26 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aditya43/golang-bookstore_users-api/utils/env"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var DBClient *sql.DB
+var (
+	DBClient *sql.DB
+
+	db_username = env.Get("MYSQL_DB_USERNAME")
+	db_password = env.Get("MYSQL_DB_PASSWORD")
+	db_host     = env.Get("MYSQL_DB_HOST")
+	db_schema   = env.Get("MYSQL_DB_SCHEMA")
+)
 
 func init() {
+
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
-		"root",
-		"adi123",
-		"127.0.0.1:3306",
-		"users_db",
+		db_username,
+		db_password,
+		db_host,
+		db_schema,
 	)
 
 	var err error
