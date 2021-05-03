b5abe598486ab9e371a2f70b4838a489a49a7320
Aditya Hajare <aditya43@gmail.com>
2021-05-03 17:55:22 +0530

    Custom Marshall

---
+++ domain/users/user_mashaller.go
@@ -0,0 +1,40 @@
+package users
+
+import "encoding/json"
+
+type PublicUser struct {
+	Id        int64  `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	// Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+	Status      string `json:"status"`
+	// Password    string `json:"-"` // To exclude password field when dealing with JSON
+}
+
+type PrivateUser struct {
+	Id          int64  `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Email       string `json:"email"`
+	DateCreated string `json:"date_created"`
+	Status      string `json:"status"`
+	Password    string `json:"-"` // To exclude password field when dealing with JSON
+}
+
+func (user *User) Marshall(isPublic bool) interface{} {
+	if isPublic {
+		return PublicUser{
+			Id:          user.Id,
+			FirstName:   user.FirstName,
+			LastName:    user.LastName,
+			DateCreated: user.DateCreated,
+			Status:      user.Status,
+		}
+	}
+
+	userJSON, _ := json.Marshal(user)
+	var privateUser PrivateUser
+	_ = json.Unmarshal(userJSON, &privateUser)
+	return privateUser
+}
