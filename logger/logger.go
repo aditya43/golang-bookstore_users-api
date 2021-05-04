d842dd7fbaaf0fe3c1442b6be1bcd4d84483ba60
Aditya Hajare <aditya43@gmail.com>
2021-05-04 10:37:53 +0530

    Switched to LowercaseLevelEncoder

--- logger/logger.go
+++ logger/logger.go
@@ -13,11 +13,12 @@ func init() {
 		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
 		Encoding:    "json",
 		EncoderConfig: zapcore.EncoderConfig{
-			LevelKey:     "level",
-			TimeKey:      "time",
-			MessageKey:   "msg",
-			EncodeTime:   zapcore.ISO8601TimeEncoder,
-			EncodeLevel:  zapcore.LowercaseColorLevelEncoder,
+			LevelKey:   "level",
+			TimeKey:    "time",
+			MessageKey: "msg",
+			EncodeTime: zapcore.ISO8601TimeEncoder,
+			// EncodeLevel:  zapcore.LowercaseColorLevelEncoder,
+			EncodeLevel:  zapcore.LowercaseLevelEncoder,
 			EncodeCaller: zapcore.ShortCallerEncoder,
 		},
 	}
