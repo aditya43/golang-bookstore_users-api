1a597cdbcc54d23e8a4cf65691a196856c4f7e18
Aditya Hajare <aditya43@gmail.com>
2021-05-04 14:31:38 +0530

    Zap logger implemented

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"github.com/aditya43/golang-bookstore_users-api/utils/env"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -8,28 +9,71 @@ import (
 var log *zap.Logger
 
 func init() {
-	logConfig := zap.Config{
-		OutputPaths: []string{"stdout"},
-		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
-		Encoding:    "json",
-		EncoderConfig: zapcore.EncoderConfig{
-			LevelKey:   "level",
-			TimeKey:    "time",
-			MessageKey: "msg",
-			EncodeTime: zapcore.ISO8601TimeEncoder,
-			// EncodeLevel:  zapcore.LowercaseColorLevelEncoder,
-			EncodeLevel:  zapcore.LowercaseLevelEncoder,
-			EncodeCaller: zapcore.ShortCallerEncoder,
-		},
+	if env.Get("GIN_MODE") == "release" {
+		buildProductionLogger()
 	}
 
-	var err error
-	if log, err = logConfig.Build(); err != nil {
-		panic(err)
+	if env.Get("GIN_MODE") != "release" {
+		buildDevelopmentLogger()
 	}
 }
 
 func Info(msg string, tags ...zap.Field) {
 	log.Info(msg, tags...)
-	log.Sync()
+	_ = log.Sync()
+}
+
+func Error(err error, tags ...zap.Field) {
+	tags = append(tags, zap.NamedError("error", err))
+	log.Error(err.Error(), tags...)
+	_ = log.Sync()
+}
+
+func buildDevelopmentLogger() {
+	config := zap.NewDevelopmentConfig()
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	opts := []zap.Option{
+		zap.AddCallerSkip(1),
+		zap.AddCaller(),
+	}
+
+	var err error
+	if log, err = config.Build(opts...); err != nil {
+		panic(err)
+	}
+}
+
+func buildProductionLogger() {
+	config := zap.NewProductionConfig()
+	config.Encoding = "json"
+	opts := []zap.Option{
+		zap.AddCallerSkip(1),
+		zap.AddCaller(),
+	}
+
+	var err error
+	if log, err = config.Build(opts...); err != nil {
+		panic(err)
+	}
+
+	// // Custom Config
+	// logConfig := zap.Config{
+	// 	OutputPaths: []string{"stdout"},
+	// 	Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+	// 	Encoding:    "json",
+	// 	DisableCaller: false,
+	// 	EncoderConfig: zapcore.EncoderConfig{
+	// 		LevelKey:    "level",
+	// 		TimeKey:     "time",
+	// 		MessageKey:  "msg",
+	// 		EncodeTime:  zapcore.ISO8601TimeEncoder,
+	// 		EncodeLevel:  zapcore.LowercaseLevelEncoder,
+	// 		EncodeCaller: zapcore.ShortCallerEncoder,
+	// 	},
+	// }
+
+	// var err error
+	// if log, err = logConfig.Build(); err != nil {
+	// 	panic(err)
+	// }
 }
