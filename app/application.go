f86dbb276a5bf68f776b6e09ac0410fa2be2d87c
Aditya Hajare <aditya43@gmail.com>
2021-05-04 10:38:04 +0530

    Added logging

--- app/application.go
+++ app/application.go
@@ -1,6 +1,9 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/aditya43/golang-bookstore_users-api/logger"
+	"github.com/gin-gonic/gin"
+)
 
 var (
 	router = gin.Default()
@@ -8,5 +11,6 @@ var (
 
 func StartApplication() {
 	mapUrls()
+	logger.Log.Info("Application started. Listening on port 8080")
 	_ = router.Run(":8080")
 }
